629004759755626028629d036b5c836bcfbf5b6d
gaugsouza <guilhermeguis@outlook.com>
2019-12-06 01:28:05 -0300

    fixed reflink

--- src/util/autenticacao.js
+++ src/util/autenticacao.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const saveUser = usuario => axios.post('https://funancial-backend.herokuapp.com/login/cadastro', usuario);
-const logarUsuario = usuario => axios.post('https://funancial-backend.herokuapp.com/login', usuario);
+const logarUsuario = usuario => axios.post('https://funancial-backend.herokuapp.com/login/', usuario);
 
 export {
     saveUser,
