86a881d8ccaddaa8cb2f7ebf7ffae9a020e7422b
natalianishimura <nataliaharumi@hotmail.com>
2019-12-11 22:14:21 -0300

    Adicionando Perfil e Estatistica

--- cypress/integration/common/clicoSubmit.js
+++ cypress/integration/common/clicoSubmit.js
@@ -3,3 +3,7 @@ import {When}  from 'cypress-cucumber-preprocessor/steps';
 When('Clico no botão submit', () =>{
     cy.get('input[type="submit"]').click();
 })
+
+When ('Clico no botão com texto {string}', (string) =>{
+    cy.get(string).click();
+})
